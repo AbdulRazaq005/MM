12e8c54cc6b96ee1c115cc860e86f26418445bb1
Abdul Razaq <razaqabd005@gmail.com>
2023-10-09 15:51:48 +0530

    move express initial point to root folder

--- backend/index.js
+++ backend/index.js
@@ -1,42 +1,42 @@
-import path from "path";
-import express from "express";
-import dotenv from "dotenv";
-import connectDatabase from "./config/dbConfig.js";
-import cookieParser from "cookie-parser";
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-import userRoutes from "./routes/userRoutes.js";
-import projectRoutes from "./routes/projectRoutes.js";
-import contactRoutes from "./routes/contactRoutes.js";
-import transactionRoutes from "./routes/transactionRoutes.js";
+// import path from "path";
+// import express from "express";
+// import dotenv from "dotenv";
+// import connectDatabase from "./config/dbConfig.js";
+// import cookieParser from "cookie-parser";
+// import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+// import userRoutes from "./routes/userRoutes.js";
+// import projectRoutes from "./routes/projectRoutes.js";
+// import contactRoutes from "./routes/contactRoutes.js";
+// import transactionRoutes from "./routes/transactionRoutes.js";
 
-dotenv.config();
-connectDatabase();
+// dotenv.config();
+// connectDatabase();
 
-const app = express();
+// const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use("/api", userRoutes);
-app.use("/api/projects", projectRoutes);
-app.use("/api/contacts", contactRoutes);
-app.use("/api/transactions", transactionRoutes);
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+// app.use("/api", userRoutes);
+// app.use("/api/projects", projectRoutes);
+// app.use("/api/contacts", contactRoutes);
+// app.use("/api/transactions", transactionRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   const __dirname = path.resolve();
+//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running....");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running....");
+//   });
+// }
 
-app.use(notFound);
-app.use(errorHandler);
+// app.use(notFound);
+// app.use(errorHandler);
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server started on port: ${port}`));
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => console.log(`Server started on port: ${port}`));
---
+++ index.js
@@ -0,0 +1,45 @@
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import connectDatabase from "./backend/config/dbConfig.js";
+import cookieParser from "cookie-parser";
+import {
+  notFound,
+  errorHandler,
+} from "./backend/middleware/errorMiddleware.js";
+import userRoutes from "./backend/routes/userRoutes.js";
+import projectRoutes from "./backend/routes/projectRoutes.js";
+import contactRoutes from "./backend/routes/contactRoutes.js";
+import transactionRoutes from "./backend/routes/transactionRoutes.js";
+
+dotenv.config();
+connectDatabase();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use("/api", userRoutes);
+app.use("/api/projects", projectRoutes);
+app.use("/api/contacts", contactRoutes);
+app.use("/api/transactions", transactionRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port: ${port}`));
